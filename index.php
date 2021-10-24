7803adac811996247464aad4ff3cf0e48926bd21
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-10-24 11:42:54 -0300

    integracion de fondo a landingPage y card con boton pra login  e info de Tales Application

--- index.php
+++ index.php
@@ -4,9 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    
 </head>
 <body>
+
+    <body style="background-image: url(img/playCubos.jpg);" >
+        <div><br><br><br><br><br><br><br><br><br><br></div>
+        <div class="align-content-center">
+    
+            <div class="card text-center bg-dark bg-opacity-25 text-white " >
+                <div class="card-header">
+                    Pagina En Contruccion
+                </div>
+                <div class="card-body">
+                  <h1 class="card-title">Tales Application</h1>
+                  <p class="card-text">aplicacion de gestion contenido audiovisual para miños </p>
+                  <a href="#" class="btn btn-primary">Iniciar Sesion</a>
+                </div>
+                <div class="card-footer ">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio similique sit inventore sint ipsa. Error sequi aspernatur dolores dignissimos, nobis odit nisi cupiditate placeat ipsa, harum dolor odio dolorum nulla.
+                </div>
+              </div>
+        </div>
+    
+        
+    </body>
     
 </body>
 </html>
