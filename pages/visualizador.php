47715612a915850b1e84cf1020a3973699186767
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-10-24 18:08:34 -0300

    agregar fondo , reproductor y boton para regresar a la pagina principal

---
+++ pages/visualizador.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>visualizacion</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+
+</head>
+
+<body style="background-image: url(../img/img1.jpg);">
+    <div class="container">
+        <button type="button" class="btn btn-light">volver a la pagina principal</button>
+
+        <div class="justify-content-center align-items-center">
+            <iframe src="" width="1280" height="720" frameborder="0"></iframe>
+        </div>
+
+    </div>
+
+
+
+
+</body>
+
+</html>
