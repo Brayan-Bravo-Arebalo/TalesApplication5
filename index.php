bfe5e9e5642f2501f4d4788780c86b49deb8c563
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-10-24 12:03:12 -0300

    eliminacion de body del index sobrante (el index tenia dos etiquetas body)

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    
-</head>
-<body>
 
-    <body style="background-image: url(img/playCubos.jpg);" >
-        <div><br><br><br><br><br><br><br><br><br><br></div>
-        <div class="align-content-center">
-    
-            <div class="card text-center bg-dark bg-opacity-25 text-white " >
-                <div class="card-header">
-                    Pagina En Contruccion
-                </div>
-                <div class="card-body">
-                  <h1 class="card-title">Tales Application</h1>
-                  <p class="card-text">aplicacion de gestion contenido audiovisual para miños </p>
-                  <a href="#" class="btn btn-primary">Iniciar Sesion</a>
-                </div>
-                <div class="card-footer ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio similique sit inventore sint ipsa. Error sequi aspernatur dolores dignissimos, nobis odit nisi cupiditate placeat ipsa, harum dolor odio dolorum nulla.
-                </div>
-              </div>
+</head>
+
+
+<body style="background-image: url(img/playCubos.jpg);">
+    <div><br><br><br><br><br><br><br><br><br><br></div>
+    <div class="align-content-center">
+
+        <div class="card text-center bg-dark bg-opacity-25 text-white ">
+            <div class="card-header">
+                Pagina En Contruccion
+            </div>
+            <div class="card-body">
+                <h1 class="card-title">Tales Application</h1>
+                <p class="card-text">aplicacion de gestion contenido audiovisual para miños </p>
+                <a href="#" class="btn btn-primary">Iniciar Sesion</a>
+            </div>
+            <div class="card-footer ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio similique sit inventore sint ipsa. Error sequi aspernatur dolores dignissimos, nobis odit nisi cupiditate placeat ipsa, harum dolor odio dolorum nulla.
+            </div>
         </div>
-    
-        
-    </body>
-    
+    </div>
+
+
 </body>
+
+
 </html>
