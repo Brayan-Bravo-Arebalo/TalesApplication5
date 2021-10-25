3f8c7589205a8d421f25f65b66053f8e9072e7a2
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-10-25 11:21:30 -0300

    modificacion de formulario para que funciones el login

--- pages/loginPage.php
+++ pages/loginPage.php
@@ -17,15 +17,15 @@
         <div class="bg-dark bg-opacity-25 text-white col-md-5 justify-content-center align-items-center">
 
             <br><br>
-            <form class="text-center">
-                <div class="mb-3">
+            <form class="text-center" action="../controllerAndModel/login/loginController.php" method="POST">
+            <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <input type="email" id="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                   <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <input type="password" id="passwors" name="password" class="form-control" id="exampleInputPassword1">
                   <div id="passHelp" class="form-text">We'll never share your password with anyone else.</div>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
