e3b79dd90da4a376c06acc013e661440988fa331
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-10-28 19:25:21 -0300

    integracion de pagina para videos en ingles

---
+++ controllerAndModel/llamarVideos/llamarInglesController.php
@@ -0,0 +1,30 @@
+<?php
+
+include "llamarVideosModel.php";
+
+
+$valores = buscar();
+if (!empty($valores)) {
+    $mostrarValores = json_decode($valores);
+    echo "<div hidden >" . $valores . " </div>";
+    echo '<div hidden>';
+    echo '<form action="../../pages/ingles.php" method="POST">
+            <input type="text" id="objeto" name="objeto">
+            <button id="btnFantasma2" type="submit"></button>
+        </form>';
+    echo '</div>';
+
+
+
+
+
+
+    echo '<script src="../../js/mostrarVideos.js"></script>';
+
+
+
+    die();
+} else {
+    echo "error";
+    die();
+}
